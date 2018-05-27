4475c6651270dd9849eba74497979dec213c7b94
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-27 19:10:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,22 @@
 class Song
   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+  end
+
+  attr_accessor :naem, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
     
   end
 end
